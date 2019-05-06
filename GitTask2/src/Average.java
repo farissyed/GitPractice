f4877677fb270bb62932cb0bbb1caf211abc5185
Upneet <captainus55@gmail.com>
2019-05-06 13:51:03 -0400

    I fixed Faris's stupid useless files that don't do anything correctly

--- GitTask2/src/Average.java
+++ GitTask2/src/Average.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Average {
 	
 	public static void main(String[] args) {
-		Scanner s = new Scaner(System.out);
-		System.in.println("Type one integer");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Type one integer");
 		int a = s.nextInt();
 		System.out.println("Type another integer");		
-		int b = Scanner.nextInt();
-		int avg = a + b / 2; 
+		int b = s.nextInt();
+		int avg = (a + b) / 2; 
 		System.out.println("This is the average of the two numbers: " + avg);
 	}
 }
