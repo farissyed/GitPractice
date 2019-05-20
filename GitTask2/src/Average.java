211f2311962036c207a20d5420cff17d6adfc8d3
Upneet <captainus55@gmail.com>
2019-05-20 17:43:51 -0400

    Added comments

--- GitTask2/src/Average.java
+++ GitTask2/src/Average.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Average {
-	
+
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+		Scanner s = new Scanner(System.in); // It should be System.in, not System.out
 		System.out.println("Type one integer");
 		int a = s.nextInt();
-		System.out.println("Type another integer");		
+		System.out.println("Type another integer");
 		int b = s.nextInt();
-		int avg = (a + b) / 2; 
-		System.out.println("This is the average of the two numbers: " + avg);
+		int avg = (a + b) / 2; // You should at least take the average of the correct numbers.
+		System.out.println("This is the average of the two numbers: " + avg); // You weren't even printing out the right
+																																					// value. I changed it to actually print out
+																																					// the average.
 	}
 }
